5b7f4e55868f50998c6066e8e122eb023d146a42
JoaoVitorBaggio <72949356+JoaoVitorBaggio@users.noreply.github.com>
2024-08-18 20:59:05 -0300

    fazendo elevador

--- simulation/qsim/work/@n@e@a@n@d@e@r/_primary.vhd
+++ simulation/qsim/work/@n@e@a@n@d@e@r/_primary.vhd
@@ -4,16 +4,16 @@ entity NEANDER is
     port(
         N               : out    vl_logic;
         RI              : out    vl_logic_vector(7 downto 0);
-        charge_RI       : out    vl_logic;
         Reset           : in     vl_logic;
         CLK_IN          : in     vl_logic;
         DEBUG           : in     vl_logic;
         step            : in     vl_logic;
-        RDM             : out    vl_logic_vector(7 downto 0);
-        charge_RDM      : out    vl_logic;
+        \REM\           : out    vl_logic_vector(7 downto 0);
+        charge_REM      : out    vl_logic;
+        AUX             : out    vl_logic_vector(7 downto 0);
+        sel             : out    vl_logic;
         PC              : out    vl_logic_vector(7 downto 0);
         Z               : out    vl_logic;
-        NOP             : out    vl_logic;
         HLT             : out    vl_logic;
         AC              : out    vl_logic_vector(7 downto 0)
     );
