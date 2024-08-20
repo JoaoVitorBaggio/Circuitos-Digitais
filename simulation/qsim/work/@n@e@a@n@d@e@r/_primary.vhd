9940cc1f7b997c2ab651a30d13b5a3ffb5931556
JoaoVitorBaggio <72949356+JoaoVitorBaggio@users.noreply.github.com>
2024-08-20 10:03:52 -0300

    Pronto

--- simulation/qsim/work/@n@e@a@n@d@e@r/_primary.vhd
+++ simulation/qsim/work/@n@e@a@n@d@e@r/_primary.vhd
@@ -3,15 +3,10 @@ use verilog.vl_types.all;
 entity NEANDER is
     port(
         N               : out    vl_logic;
-        RI              : out    vl_logic_vector(7 downto 0);
         Reset           : in     vl_logic;
         CLK_IN          : in     vl_logic;
-        DEBUG           : in     vl_logic;
         step            : in     vl_logic;
-        \REM\           : out    vl_logic_vector(7 downto 0);
-        charge_REM      : out    vl_logic;
-        AUX             : out    vl_logic_vector(7 downto 0);
-        sel             : out    vl_logic;
+        DEBUG           : in     vl_logic;
         PC              : out    vl_logic_vector(7 downto 0);
         Z               : out    vl_logic;
         HLT             : out    vl_logic;
--- simulation/qsim/work/@n@e@a@n@d@e@r_vlg_check_tst/_primary.vhd
+++ simulation/qsim/work/@n@e@a@n@d@e@r_vlg_check_tst/_primary.vhd
@@ -3,14 +3,9 @@ use verilog.vl_types.all;
 entity NEANDER_vlg_check_tst is
     port(
         AC              : in     vl_logic_vector(7 downto 0);
-        AUX             : in     vl_logic_vector(7 downto 0);
-        charge_REM      : in     vl_logic;
         HLT             : in     vl_logic;
         N               : in     vl_logic;
         PC              : in     vl_logic_vector(7 downto 0);
-        \REM\           : in     vl_logic_vector(7 downto 0);
-        RI              : in     vl_logic_vector(7 downto 0);
-        sel             : in     vl_logic;
         Z               : in     vl_logic;
         sampler_rx      : in     vl_logic
     );
